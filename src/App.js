ffd5a0710c70acc26d365bb3553701faa8f854ad
Surya <surya4285696@gmail.com>
2023-12-22 17:16:04 -1200

    advise generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import icondice from "./images/icon-dice.svg";
+import destopdivider from "./images/pattern-divider-desktop.svg";
+import mobiledivider from "./images/pattern-divider-mobile.svg";
 
 function App() {
+  const [Advise, setadvise] = useState({
+    slip: {
+      id: 117,
+      advice:
+        "It is easy to sit up and take notice, what's difficult is getting up and taking actions.",
+    },
+  });
+  const width = window.innerWidth;
+  const time = new Date();
+  const url = `https://api.adviceslip.com/advice?timestramp=${time.getTime()}`;
+  async function fetchadvise() {
+    const response = await fetch(url);
+    const theadvise = await response.json();
+    setadvise(theadvise);
+  }
+
+  const advisesetter = () => {
+    fetchadvise();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <main>
+        <h4 className="advise-id">Advise # {Advise.slip.id}</h4>
+        <p className="advise-content">"{Advise.slip.advice}"</p>
+        <img
+          src={width >= 500 ? destopdivider : mobiledivider}
+          alt="none"
+          className="seperator-img"
+        />
+        <button onClick={advisesetter}>
+          <img src={icondice} alt="icondice" />
+        </button>
+      </main>
     </div>
   );
 }
